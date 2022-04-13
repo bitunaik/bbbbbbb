8125abc13b287fde594293694077affdf65e4d8d
bitunaik1 <bitunaik123@gmail.com>
2022-04-13 13:02:31 +0530

    g

--- Java Practice/src/occuranceword/Test8.java
+++ Java Practice/src/occuranceword/Test8.java
@@ -1,7 +1,5 @@
 package occuranceword;
 
-import java.util.LinkedHashSet;
-
 public class Test8 {
 public static void main(String[] args) {
 	String s="khatam tata bye bye";
---
+++ Java Practice/src/occuranceword/Test9.java
@@ -0,0 +1,10 @@
+package occuranceword;
+
+public class Test9 {
+public static void main(String[] args) {
+	String s="khatam tata bye bye";
+	for(int i=s.length()-1;i>=0;i--) {
+		System.out.print(s.charAt(i));
+	}
+}
+}
