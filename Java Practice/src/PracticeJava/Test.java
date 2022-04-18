51f1b133cf5679c34d0bf345875459d01bd64d51
bitunaik1 <bitunaik123@gmail.com>
2022-04-18 15:33:42 +0530

    just do it

---
+++ Java Practice/src/PracticeJava/Test.java
@@ -0,0 +1,11 @@
+package PracticeJava;
+
+public class Test {
+public void m1() {
+System.out.println("hello");
+}
+final public static void main(String[] args) {
+	Test t=new Test();
+	t.m1();
+}
+}
